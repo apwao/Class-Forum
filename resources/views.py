73be687553468daf9c1f0baba2ab06289a94377b
Sowayi <sapwao@gmail.com>
2019-07-11 04:33:24 -0400

    Add homepage view_function to display resources on the homepage

--- resources/views.py
+++ resources/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+# from .forms import ProfileForm
+# from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepage(request):
@@ -6,3 +10,8 @@ def homepage(request):
     """
     
     return render(request, 'homepage.html')
+
+def create_profile(request):
+    """
+    """
+    return render(request, 'profile_form.html')
