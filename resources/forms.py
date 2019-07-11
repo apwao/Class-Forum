462c86d998ecd14edf4f5a654999e52e536811ab
Sowayi <sapwao@gmail.com>
2019-07-11 04:26:29 -0400

    Add ResourcesForm to provide Admin with a form to create resources

--- resources/forms.py
+++ resources/forms.py
@@ -1,2 +1,10 @@
-# from .models import Profile
+from .models import Resources
 from django import forms
+
+class ResourcesForm(forms.ModelForm):
+    """
+    """
+    class Meta:
+        model:Resources
+        fields=('topic','cover_image','resource_type','content','task')
+        
