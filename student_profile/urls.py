9ad9855a8fe5b25d3009c696f711680d6ca87d4e
Sowayi <sapwao@gmail.com>
2019-07-11 04:42:47 -0400

    Add url to display student profile after creating it

--- student_profile/urls.py
+++ student_profile/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns=[
     # url('^$',views.homepage, name='homepage'),
-    url('^create_profile', views.create_profile, name='create_profile'),
-    
+    url(r'^create_profile', views.create_profile, name='create_profile'),
+    url(r'^view_profile',views.view_profile,name='view_profile'),
+    url(r'^edit_profile',views.edit_profile, name='edit_profile'),
     
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
