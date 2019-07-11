f52a6dd56e9178ff93150922308a57c7b2e03294
Sowayi <sapwao@gmail.com>
2019-07-11 07:02:59 -0400

    Add a view_resources url to display available resources

--- resources/urls.py
+++ resources/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.homepage, name='homepage'),
+    url(r'^$',views.homepage, name='homepage'),
+    url(r'^view_resources', views.view_resources, name='view_resources'),
     
 ]
